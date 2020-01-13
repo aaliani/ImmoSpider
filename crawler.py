37ae627f8faee4622972572cff4f30187e636e41
Aqeel Aliani <aqeelaliani@Aqeels-MacBook-Pro.local>
2020-01-13 16:45:36 +0100

    130120 MAC

--- crawler.py
+++ crawler.py
@@ -11,6 +11,9 @@ import time
 import pandas as pd
 import numpy as np
 
+import configparser
+
+
 CHROMEDRIVER_PATH = '/usr/bin/chromedriver'
 CHROMEDRIVER_PATH = '/Applications/chromedriver'
 
@@ -21,15 +24,25 @@ chrome_options.add_argument("--test-type")
 
 # driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
 
+config = configparser.ConfigParser()
+inifile = 'config.ini'
+config.read(inifile)
+format = "%Y-%m-%dT%H:%M:%S"
+
 for i in range(5):
-    t = datetime.now()
+    t = datetime.now().strftime(format)
     # bashCommand = "cwm --rdf test.rdf --ntriples > test.nt"
     bashCommand = "echo '%s'" % t
     bashCommand = "ls"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     # output, error = process.communicate()
-    print(process.communicate())
+    # print(process.communicate())
+    print(config['global']['last_queried'])
+    config['global']['last_queried'] = t
     time.sleep(1)
+
+with open(inifile, 'w') as configfile:
+    config.write(configfile)
 
 # driver.close
 
